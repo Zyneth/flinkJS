787320c220487d5f2cef2290a99e032dbd19038d
Resit <guerkan.resit@gmail.com>
2021-10-09 15:19:07 +0200

    added WIP / to-do

--- flink.js
+++ flink.js
@@ -17,7 +17,7 @@ axios({
   }
 }).then(response => {
    for (i in response.data.swim_lanes[0].products) { // looping new products
-      if (response.data.swim_lanes[0].products[i].id){
+      if (response.data.swim_lanes[0].products[i].id){ // --WIP-- filtering by id -> if id doesnt exist in our object add it with a notification
 
         obj["Products"].push({'id': response.data.swim_lanes[0].products[i].id,  // add ID, name, desc and price in our JSON object
                               'name':response.data.swim_lanes[0].products[i].name,
