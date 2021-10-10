59b8fa9788979570956250a5ebffcc3df1f9bef2
Resit <guerkan.resit@gmail.com>
2021-10-10 13:52:27 +0200

    implemented id filter

--- flink.js
+++ flink.js
@@ -3,11 +3,6 @@ var axios = require('axios');
 var fs = require('fs');
 
 
-
-
-var data = '{"Products":[]}' // json structure
-var obj = JSON.parse(data); // json init
-
 axios({
   method: 'get', // http call
   url: 'https://www.goflink.com/api-proxy/home', // flink api
@@ -16,21 +11,23 @@ axios({
     'hub-slug': process.env.FLINKLOCATION // nearby flink location (example: de_ber_wilm)
   }
 }).then(response => {
-   for (i in response.data.swim_lanes[0].products) { // looping new products
-      if (response.data.swim_lanes[0].products[i].id){ // --WIP-- filtering by id -> if id doesnt exist in our object add it with a notification
+          fs.readFile('products.json', 'utf-8', function(err, data) {
+            var arrayOfObjects = JSON.parse(data)
+          	if (err) throw err
+            for (i in response.data.swim_lanes[0].products) { // loop products array
+              if (response.data.swim_lanes[0].products[i].id != arrayOfObjects.Products[i].id){ // check if id already exists
+                arrayOfObjects.Products.push({
+                  'id': response.data.swim_lanes[0].products[i].id,  // add ID, name, desc and price in our JSON object
+                 'name':response.data.swim_lanes[0].products[i].name,
+                 'description':response.data.swim_lanes[0].products[i].description,
+                 'price': response.data.swim_lanes[0].products[i].price.amount+' EUR'});
+                 console.log(response.data.swim_lanes[0].products[i].name+ ' added')
 
-        obj["Products"].push({'id': response.data.swim_lanes[0].products[i].id,  // add ID, name, desc and price in our JSON object
-                              'name':response.data.swim_lanes[0].products[i].name,
-                             'description':response.data.swim_lanes[0].products[i].description,
-                             'price': response.data.swim_lanes[0].products[i].price.amount+' EUR'});
-
-        console.log(response.data.swim_lanes[0].products[i].name + ' added! ')
-        
-        fs.writeFile ("products.json", JSON.stringify(obj,'id',2), function(err) { // put JSON object in an external file
-          if (err)
-            console.log(err)
-          } 
-        )
-    }
-  }
-})
+  
+                 fs.writeFile('products.json', JSON.stringify(arrayOfObjects, 'id', 2), 'utf-8', function(err) { // write to external file
+                 if (err) throw err})
+                  }
+              }
+         }
+   )}
+)
